a69cbace8ab0ebe5e123652652dbcaf85133b2d5
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-10-08 22:31:08 -0200

    Quedarse en misma posicion inicial si comando es 'B'

--- src/ControladorDeAuto.js
+++ src/ControladorDeAuto.js
@@ -1,4 +1,5 @@
 function controlarAuto(cadenaDeControlAuto) {
+  if (cadenaDeControlAuto == "B") return "(4,0)N"
   return "(4,1)N";
 }
 
--- src/ControladorDeAuto.test.js
+++ src/ControladorDeAuto.test.js
@@ -4,4 +4,9 @@ describe("controlarAuto", () => {
   it("deberia avanzar una posición del auto con superficie y posicion inicial por defecto", () => {
     expect(controlarAuto("A")).toEqual("(4,1)N");
   });
+  it("deberia quedarse en la misma posicion para comando B", () => {
+    expect(controlarAuto("B")).toEqual("(4,0)N");
+  });
+
+ 
 });
